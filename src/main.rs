190a138b44ab694c2ff8ccc79b2ff171229124cd
mrtnvgr <martynovegorOF@yandex.ru>
2025-10-25 20:06:08 +0700

    chore: rewrite some logic for easy integration of new formats

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use anyhow::Result;
 use clap::Parser;
 use ini::Ini;
 use quiet_panics::set_panic_hook;
-use std::mem::discriminant;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use strum::{EnumIter, IntoEnumIterator};
+
+macro_rules! return_if_ok {
+    ($result:expr) => {{
+        if let Ok(x) = $result {
+            return x;
+        }
+    }};
+}
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -23,17 +32,28 @@ enum Data {
     Ini(Ini),
 }
 
-impl File {
-    fn load(path: PathBuf) -> Self {
-        let data: Data;
+#[derive(PartialEq, EnumIter)]
+enum Format {
+    Ini,
+}
 
-        if let Ok(ini) = Ini::load_from_file(&path) {
-            data = Data::Ini(ini);
-        } else {
-            panic!("{} format is unsupported", path.display());
+impl File {
+    fn load(path: &Path) -> Self {
+        for format in Format::iter() {
+            let result = Self::load_as(path, &format);
+            return_if_ok!(result);
         }
 
-        Self { path, data }
+        panic!("{} format is unsupported", path.display());
+    }
+
+    fn load_as(path: &Path, format: &Format) -> Result<Self> {
+        let data = match format {
+            Format::Ini => Data::Ini(Ini::load_from_file(path)?),
+        };
+
+        let path = path.to_path_buf();
+        Ok(Self { path, data })
     }
 
     fn compatible(&self, x: &Self) -> bool {
@@ -50,8 +70,14 @@ impl File {
 }
 
 impl Data {
+    const fn format(&self) -> Format {
+        match self {
+            Self::Ini(_) => Format::Ini,
+        }
+    }
+
     fn compatible(&self, x: &Self) -> bool {
-        discriminant(self) == discriminant(x)
+        self.format() == x.format()
     }
 
     fn apply(&mut self, patch: &Self) {
@@ -91,17 +117,17 @@ fn main() {
         assert!(patch.is_file(), "{} is not a file", patch.display());
     }
 
-    let mut target = File::load(args.target);
+    let patches: Vec<File> = args.patches.iter().map(|x| File::load(x)).collect();
+    let patch_format = get_patch_format(&patches);
 
-    let mut patches: Vec<File> = vec![];
-    for patch in args.patches {
-        let patch = File::load(patch);
+    let target = File::load_as(&args.target, &patch_format);
+    let mut target = target.expect("Failed to load target");
 
-        let compatible = target.compatible(&patch);
+    for patch in &patches {
+        let compatible = target.compatible(patch);
+
         let patch_path = patch.path.display();
         assert!(compatible, "{patch_path} is not valid");
-
-        patches.push(patch);
     }
 
     for patch in patches {
@@ -109,4 +135,14 @@ fn main() {
     }
 
     target.save();
+}
+
+#[allow(clippy::indexing_slicing)]
+fn get_patch_format(items: &[File]) -> Format {
+    let first = items[0].data.format();
+
+    let are_same = items.iter().all(|x| x.data.format() == first);
+    assert!(are_same, "Patches have different file formats");
+
+    first
 }
